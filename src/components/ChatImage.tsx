88ab8e9fe079d6ade054b7504e3cda7aa767a8f7
extra <extrahash@protonmail.com>
2020-07-14 00:49:42 -0600

    remove axios references

--- src/components/ChatImage.tsx
+++ src/components/ChatImage.tsx
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react';
-import ax from 'axios';
 
 type State = {};
 
--- src/components/ChatLink.tsx
+++ src/components/ChatLink.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ax from 'axios';
 
 type State = {};
 
