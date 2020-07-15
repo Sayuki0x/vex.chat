d94a6a091b75591f5e7f9f0c4d034c34bf47c842
ExtraHash <ExtraHash@protonmail.com>
2020-07-14 18:39:28 -0600

    fix linting errors and bump libvex

--- src/components/ChatBlockquote.tsx
+++ src/components/ChatBlockquote.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ReactNode } from 'react';
+import React, { Component } from 'react';
 import { faGreaterThan } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
